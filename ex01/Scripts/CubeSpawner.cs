4521e1bde4a0cb90d23fddcbb1de8a69b872a00a
Emilien Baudet <emilien.baudet@gmail.com>
2019-03-26 12:26:42 +0100

    blwer

--- ex01/Scripts/CubeSpawner.cs
+++ ex01/Scripts/CubeSpawner.cs
@@ -6,10 +6,16 @@ public class CubeSpawner : MonoBehaviour {
 	public GameObject	cube;
 	public float		spawnTime;
 	public float 		timer;
-	// Update is called once per frame
-	void Update () {
+
+    //private void Start()
+    //{
+    //    Destroy(gameObject);
+    //}
+    // Update is called once per frame
+    void Update () {
 		if (timer > spawnTime)
 		{
+            //Destroy(gameObject);
 			timer = 0;
             //Vector3 newPos = new Vector3(cube.transform.localPosition.x, cube.transform.localPosition.y, cube.transform.localPosition.z);
             GameObject.Instantiate(cube, cube.transform.localPosition, Quaternion.identity);
