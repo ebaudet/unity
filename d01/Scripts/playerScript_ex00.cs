cfa567cbf08851b69b94bb5e5daa20475789ec46
Emilien BAUDET <ebaudet@e1r5p17.42.fr>
2019-03-27 16:47:12 +0100

    sript ex00

--- d01/Scripts/playerScript_ex00.cs
+++ d01/Scripts/playerScript_ex00.cs
@@ -2,15 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerScript_ex00 : MonoBehaviour {
+public class playerScript_ex00 : MonoBehaviour
+{
 
+	public GameObject player;
+	public string key;
+	public bool isActive;
+
+	private GameObject[] _players;
+	
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+		GameObject.FindGameObjectsWithTag("player");
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		SelectPlayer();
 		
+	}
+
+	private void SelectPlayer()
+	{
+		if (Input.GetKeyDown(key))
+		{
+			foreach (GameObject _player in _players)
+			{
+				_player.GetComponent<isActive>() = false;
+			}
+
+			isActive = true;
+		}
 	}
 }
